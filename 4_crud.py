d37f96afebe2faad128dd5c87b4ac9f2e3a4e216
tyagishivamkk512-oss <tyagishivamkk512@gmail.com>
2026-08-21 03:58:17 +0530

    Add error handling for post deletion and updating; refactor find_post_index function

--- 4_crud.py
+++ 4_crud.py
@@ -24,10 +24,18 @@ def find_post(id):
         if i['id'] == id:
             return i
 
+def find_post_index(id):
+    for i,p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+#Get all Posts
 @app.get("/posts")
 def get_posts():
     return {"data":my_posts}
 
+
+#Create Post
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post : Post):
     post_dict = post.dict()
@@ -35,6 +43,7 @@ def create_posts(post : Post):
     my_posts.append(post_dict)
     return {"data":my_posts}
 
+#Get specific Post
 @app.get("/posts/{id}")
 def get_post(id:int):
     post = find_post(id)
@@ -46,3 +55,28 @@ def get_post(id:int):
         #return {f"post with id: {id} wasn't found"}
 
     return {"post details":post}
+
+
+#Delete Post
+@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def get_post(id:int):
+    index = find_post_index(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with {id} id does not exist!!")
+    
+    my_posts.pop(index)
+    return {"message":"Post deleted successfully"}
+
+#Update Post
+@app.put("/posts/{id}")
+def update_post(id:int, post : Post):
+    index = find_post_index(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with {id} id does not exist!!")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data" : post_dict}
